ce334a7e2915904b5638a4899af6996fd384e34c
geovanips <geovanips@outlook.com>
2014-08-17 11:15:29 -0300

    par_ou_impar

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "\n ###PROGRAMA PARA VERIFICAR UM NUMERO PAR OU IMPAR####\n";
+    cout << "Digite o numero para verificar se e par ou impar:\n";
+    cin >> a;
+    if(a%2==0)
+        cout << "\nO numero e par";
+    else
+
+        if (a%2!=0)
+                cout << "\n o numero e impar\n";
+      return 0;
+}
